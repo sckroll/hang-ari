39d96e5bda3fbdd9add0a754c490c503310a63a5
sckroll <kimsc0714@gmail.com>
2021-01-21 17:04:26 +0900

    Feat: 각 라우트를 통합하는 파일 추가

--- src/api/index.js
+++ src/api/index.js
@@ -1,9 +1,18 @@
+/**
+ * API 라우트 통합 파일
+ *
+ * @author Sckroll
+ */
+
 import { Router } from 'express'
+import auth from './auth'
+// import club from './club'
+// import post from './post'
 
 const router = Router()
 
-router.get('/', (req, res) => {
-  res.send('hello')
-})
+router.use('/auth', auth)
+// router.use('/club', club)
+// router.use('/post', post)
 
 export default router
