9ff4cc2ffcdf587e0be50e079a1b5351962e1908
sckroll <kimsc0714@gmail.com>
2021-03-05 23:28:37 +0900

    Feat: 버튼 컴포넌트 구현 및 샘플 페이지 추가

---
+++ frontend/src/components/common/Button.vue
@@ -0,0 +1,117 @@
+<template>
+  <button :class="getSize">
+    <slot></slot>
+  </button>
+</template>
+
+<script>
+export default {
+  props: {
+    sm1: Boolean,
+    sm2: Boolean,
+    sm3: Boolean,
+    md1: Boolean,
+    md2: Boolean,
+    md3: Boolean,
+    lg1: Boolean,
+    lg2: Boolean,
+    lg3: Boolean,
+  },
+  data() {
+    return {
+      defaultSize: 'medium-2',
+      sizes: new Map([
+        ['sm1', 'small-1'],
+        ['sm2', 'small-2'],
+        ['sm3', 'small-3'],
+        ['md1', 'medium-1'],
+        ['md2', 'medium-2'],
+        ['md3', 'medium-3'],
+        ['lg1', 'large-1'],
+        ['lg2', 'large-2'],
+        ['lg3', 'large-3'],
+      ]),
+    }
+  },
+  computed: {
+    getSize() {
+      let size
+      for (const [key, value] of Object.entries(this.$props)) {
+        if (value) {
+          size = this.sizes.get(key)
+          break
+        }
+      }
+      if (!size) size = this.defaultSize
+      return size
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+button {
+  border: 0;
+  border-radius: 3px;
+  margin: 8px;
+  padding: 0;
+  cursor: pointer;
+  outline: none;
+  background-color: $default-color-1;
+  transition: all 0.2s ease;
+
+  &:hover,
+  &:focus {
+    background-color: lighten($default-color-1, 10%);
+  }
+
+  &:active {
+    background-color: darken($default-color-1, 5%);
+  }
+}
+
+.small {
+  &-1 {
+    width: 24px;
+    height: 24px;
+  }
+  &-2 {
+    width: 48px;
+    height: 24px;
+  }
+  &-3 {
+    width: 72px;
+    height: 24px;
+  }
+}
+
+.medium {
+  &-1 {
+    width: 32px;
+    height: 32px;
+  }
+  &-2 {
+    width: 64px;
+    height: 32px;
+  }
+  &-3 {
+    width: 96px;
+    height: 32px;
+  }
+}
+
+.large {
+  &-1 {
+    width: 40px;
+    height: 40px;
+  }
+  &-2 {
+    width: 80px;
+    height: 40px;
+  }
+  &-3 {
+    width: 120px;
+    height: 40px;
+  }
+}
+</style>
---
+++ frontend/src/views/ComponentsSample.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <div class="buttons">
+      <div class="row">
+        <ha-button sm1>sm1</ha-button>
+        <ha-button sm2>sm2</ha-button>
+        <ha-button sm3>sm3</ha-button>
+      </div>
+      <div class="row">
+        <ha-button md1>md1</ha-button>
+        <ha-button md2>md2</ha-button>
+        <ha-button md3>md3</ha-button>
+      </div>
+      <div class="row">
+        <ha-button lg1>lg1</ha-button>
+        <ha-button lg2>lg2</ha-button>
+        <ha-button lg3>lg3</ha-button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import haButton from '@/components/common/Button'
+
+export default {
+  components: {
+    haButton,
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.buttons {
+  padding: 8px;
+  background-color: #dddddd;
+}
+</style>
