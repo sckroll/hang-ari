6c6c882d8616c16b4fb2dd65255597fcd8bc28eb
sckroll <kimsc0714@gmail.com>
2021-03-10 23:47:59 +0900

    Feat: 입력창 컴포넌트 구현

---
+++ frontend/src/components/common/HaInput.vue
@@ -0,0 +1,43 @@
+<template>
+  <input
+    :value="value"
+    :placeholder="placeholder"
+    @input="$emit('input', $event.target.value)"
+    type="text"
+  />
+</template>
+
+<script>
+export default {
+  name: 'HaInput',
+  props: {
+    value: {
+      type: [String, Number],
+    },
+    placeholder: {
+      type: String,
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+input {
+  outline: none;
+  border-top: 3px solid transparent;
+  border-left: 0;
+  border-right: 0;
+  border-bottom: 3px solid $grey-color-3;
+  padding: 5px 8px;
+  width: 100%;
+  transition: all 0.2s ease;
+
+  &:hover {
+    background-color: $grey-color-4;
+  }
+  &:focus {
+    border-bottom: 3px solid $primary-color-0;
+    background-color: $grey-color-4;
+  }
+}
+</style>
