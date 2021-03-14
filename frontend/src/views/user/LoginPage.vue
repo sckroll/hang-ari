e8fc450eadcdde7c99d0ba8893aa296665d69a31
sckroll <kimsc0714@gmail.com>
2021-03-14 16:09:47 +0900

    Feat: 로그인 페이지 생성

---
+++ frontend/src/views/user/LoginPage.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="login-container">
+    Login page
+  </div>
+</template>
+
+<script></script>
+
+<style lang="scss" scoped>
+.login-container {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
